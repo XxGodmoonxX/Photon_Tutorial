dcebd5254d5623ebcbe89b54b0659d7ce96415ff
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-16 16:30:06 +0900

    諸々整理

---
+++ Assets/Scripts/DemoNetWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoNetWork : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings( null );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onJoinedLobby() {
+		Debug.Log( "OnJoinedLobby()" );
+	}
+}
