124a22cc7da98c130d38b75595859b81cdca733b
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-17 18:46:40 +0900

    FIx

--- Assets/Scripts/DemoNetWork.cs
+++ Assets/Scripts/DemoNetWork.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class DemoNetWork : MonoBehaviour {
 
+	//SerializeFieldはPublicと似てるけどコードで書き換えられない、Unityのエディタ上のみ。
+	[SerializeField]
+  private string  m_resourcePath  = "";
+  [SerializeField]
+  private float   m_randomCircle  = 4.0f;
+
+	private const string ROOM_NAME  = "RoomA";
+
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings( null );
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		Debug.Log("Start()");
 	}
 
 	void onJoinedLobby() {
 		Debug.Log( "OnJoinedLobby()" );
+		PhotonNetwork.JoinOrCreateRoom( ROOM_NAME, new RoomOptions(), TypedLobby.Default);
 	}
+
+	public void SpawnObject() {
+		PhotonNetwork.Instantiate( m_resourcePath, GetRandomPosition(), Quaternion.identity, 0);
+	}
+
+	private Vector3 GetRandomPosition() {
+		var rand = Random.insideUnitCircle * m_randomCircle;
+		return rand;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// SpawnObject();
+	}
+
+	
 }
---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,17 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PhotonManager : Photon.MonoBehaviour {
+
+// 	// Use this for initialization
+// 	void Start () {
+// 		PhotonNetwork.ConnectUsingSettings( null );
+// 		Debug.Log("Start");
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+		
+// 	}
+// }
