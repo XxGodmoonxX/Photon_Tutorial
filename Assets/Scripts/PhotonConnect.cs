5ffc3408892031e3720b6760226ccfe3342a6fcc
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-18 04:07:12 +0900

    Unity 2018: 2D Multiplayer Photon Tutorial [EP.1] 終わった

---
+++ Assets/Scripts/PhotonConnect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonConnect : MonoBehaviour {
+
+	public string versionName = "0.1";
+
+	public GameObject sectionview1, sectionview2, sectionview3;
+
+	public void connectToPhoton() {
+		PhotonNetwork.ConnectUsingSettings(versionName);
+		Debug.Log("Connecting to Photon...");
+	}
+
+	private void OnConnectedToMaster() {
+		PhotonNetwork.JoinLobby(TypedLobby.Default);
+		Debug.Log("Connected to master");
+	}
+
+	private void OnJoinedLobby() {
+		sectionview1.SetActive(false);
+		sectionview2.SetActive(true);
+		Debug.Log("Joinde Lobby");
+	}
+
+	private void OnDisconnectFromPhoton() {
+		if(sectionview1.active) {
+			sectionview1.SetActive(false);
+		}
+		if(sectionview2.active) {
+			sectionview1.SetActive(false);
+		}
+		sectionview3.SetActive(true);
+
+		Debug.Log("Disconnect from photon");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
